4ee189c66a9fc5e933ea77eed5ef38ffb529032e
David Kwan <dwkwan1989@gmail.com>
2019-02-07 22:57:05 +0000

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - returns 0
+ *
+ * Description - main returns 0
+ * putchar - Prints each letter of the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+  printf("%i",i);
+}
+printf("\n");
+return (0);
+}
