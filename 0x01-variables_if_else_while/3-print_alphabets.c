5acc5f0bd753aa5dd618aa7dfc79191f4b4679f7
David Kwan <dwkwan1989@gmail.com>
2019-02-07 22:44:11 +0000

    Add 3-print_alphabets.c and fix description of 4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,19 @@
  * main - returns 0
  *
  * Description - main returns 0
- * putchar - Prints each letter of the alphabet in lowercase except e and q
+ * putchar - Prints each letter of the alphabet in lowercase and uppercase
  * Return: 0
  */
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'z' ; c++)
-{
-if (c != 'e' && c != 'q')
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 }
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
 }
 putchar('\n');
 return (0);
