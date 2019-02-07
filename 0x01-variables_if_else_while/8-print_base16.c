f589adeae259450052a8da80ea407c2abbd6989e
David Kwan <dwkwan1989@gmail.com>
2019-02-07 23:50:39 +0000

    Add 8-print_base16.c and fix description for 6-print_numberz.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,17 @@
  * main - returns 0
  *
  * Description - main returns 0
- * putchar - Prints all single digit numbers of base 10
+ * putchar - Prints all the numbers of base 16 in lowercase
  * Return: 0
  */
 int main(void)
 {
 int i;
 for (i = 48 ; i < 58 ; i++)
+{
+putchar (i);
+}
+for (i = 97 ; i < 103 ; i++)
 {
 putchar (i);
 }
