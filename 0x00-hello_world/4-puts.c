ce7aad93e38d1cc50a8daaad41d3195d5cee9607
David Kwan <dwkwan1989@gmail.com>
2019-02-07 00:50:03 +0000

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - returns 0
+ *
+ * Description - main returns 0
+ * puts - Writes string and newline to stdout
+ * Return: 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
