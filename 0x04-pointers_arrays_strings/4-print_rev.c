0d141ba2de2a638649da118478bb65491d84f521
David Kwan <dwkwan1989@gmail.com>
2019-02-19 19:05:43 +0000

    Add function prototype to 4-print_rev.c

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -3,11 +3,22 @@
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: pointer to string
  */
+int _strleng(char *s);
+
 void print_rev(char *s)
 {
 	int c;
 
-	for (c = _strlen(s) -1; c >= 0; c--)
+	for (c = _strlen(s); c >= 0; c--)
 		_putchar(s[c]);
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+        int c = 0;
+
+        while (s[c] != '\0')
+                c++;
+        return (c);
 }
