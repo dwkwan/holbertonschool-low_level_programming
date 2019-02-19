76e1650e04bab6a1b5d974bbba5b6181f3dff31a
David Kwan <dwkwan1989@gmail.com>
2019-02-19 22:08:55 +0000

    Add 8-print_array.c and update holberton.h

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to string
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int c = 0;
+
+	for (c = 0; c < n - 1; c++)
+		printf("%i, ", a[c]);
+	printf("%i", a[n - 1]);
+	printf("\n");
+}
