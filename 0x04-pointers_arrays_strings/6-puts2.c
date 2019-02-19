ddafff7b3551ba8feb18d090d166da26b93e5037
David Kwan <dwkwan1989@gmail.com>
2019-02-19 20:18:49 +0000

    Update statement for loop of 6-puts2.c

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+int _strlen(char *s);
 /**
  * puts2 - prints one char out of 2 of a string, followed by a new line
  * @str: pointer to string
@@ -6,8 +7,24 @@
 void puts2(char *str)
 {
 	int c;
+	int len = _strlen(str);
 
-	for (c = 0; str[c] != '\0'; c += 2)
+	for (c = 0; c < len; c += 2)
 		_putchar(str[c]);
 	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
