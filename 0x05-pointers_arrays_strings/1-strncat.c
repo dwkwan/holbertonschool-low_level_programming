5a1e29bdf9e1b8b54afabdad456bab1e0e16138e
dwkwan <dwkwan1989@gmail.com>
2019-02-21 20:39:36 +0000

    Add 1-strncat.c and update holberton.h

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * _strncat - concatenates two strings with at most n bytes from src
+ * @dest: pointer to dest string
+ * @src: pointer to src string
+ * @n: number of bytes from src
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = _strlen(dest);
+		/**int len2 = _strlen(src);*/
+	int i = 0;
+	int c;
+
+	for (c = len1; c < len1 + n; c++, i++)
+		dest[c] = src[i];
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+	int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
