74a42df0bfa591752e2f81aa5bec6f427b1eeeb0
dwkwan <dwkwan1989@gmail.com>
2019-02-22 02:16:31 +0000

    Add 3-rev_array.c and update holberton.h

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * reverse_array - reverses the contents of an array of integers
+ * @a: pointer to string
+ * @n: number of elements in array
+ */
+void reverse_array(int *a, int n)
+{
+	int c, d;
+	int array[1000];
+	int g = n - 1;
+
+	for (d = 0; d < n; d++, g--)
+		array[d] = a[g];
+	for (c = 0; c <= n; c++)
+		a[c] = array[c];
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
