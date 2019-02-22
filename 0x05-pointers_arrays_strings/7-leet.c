0d0d5992d2a7f98cd60064a3b3f7ff360bda38e6
dwkwan <dwkwan1989@gmail.com>
2019-02-22 09:12:48 +0000

    Add 7-leet.c

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,20 +1,14 @@
 #include "holberton.h"
-int _strlen(char *s);
-/**
- * leet - capitalizes all words of a string
- * @s: pointer to string
- * Return: the pointer to the string
- */
+
 char *leet(char *s)
 {
 	int c, j;
-	int len = _strlen(s);
 	char letter[12] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l'
 				 , 'L'};
 	char number[12] = {'4', '4', '3', '3', '0', '0', '7', '7', '1'
 				 , '1'};
 
-	for (c = 0; c < len; c++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' ||
 		    s[c] == 'o' || s[c] == 'O' || s[c] == 't' || s[c] == 'T' ||
