82c4f3ca3c0fe53a2842267368280db1ab5e406a
dwkwan <dwkwan1989@gmail.com>
2019-02-22 09:02:59 +0000

    Add 7-leet.c and update holberton.h

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+int _strlen(char *s);
+/**
+ * leet - capitalizes all words of a string
+ * @s: pointer to string
+ * Return: the pointer to the string
+ */
+char *leet(char *s)
+{
+	int c, j;
+	int len = _strlen(s);
+	char letter[12] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l'
+				 , 'L'};
+	char number[12] = {'4', '4', '3', '3', '0', '0', '7', '7', '1'
+				 , '1'};
+
+	for (c = 0; c < len; c++)
+	{
+		if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' ||
+		    s[c] == 'o' || s[c] == 'O' || s[c] == 't' || s[c] == 'T' ||
+		    s[c] == 'l' || s[c] == 'L')
+		{
+			for (j = 0; s[c] != letter[j]; j++)
+			{}
+			s[c] = number[j];
+		}
+	}
+
+	return (s);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+	int _strlen(char *s)
+	{
+		int c = 0;
+
+		while (s[c] != '\0')
+			c++;
+		return (c);
+	}
