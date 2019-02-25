fa100f49534e6b13be83b336f1154b0b93a800b2
dwkwan <dwkwan1989@gmail.com>
2019-02-25 22:36:55 +0000

    Add 2-strchr.c

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 /**
  * _strchr -  locates a character in a string
+ * @s: string to check
  * @c: character to locate
- * @b: character array to check
  * Return: the pointer to first occurrence of c or return NULL
  */
 char *_strchr(char *s, char c)
 {
-        int i;
-        for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-			return s + i;
-	}
-        return 0;
+	int i = 0;
+
+	do {
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	} while (*(s + i) != '\0');
+	return (0);
 }
