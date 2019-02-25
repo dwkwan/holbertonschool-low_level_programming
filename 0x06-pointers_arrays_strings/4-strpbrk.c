0c0102b7d775e9adcb513a8435ade42f58c26643
dwkwan <dwkwan1989@gmail.com>
2019-02-25 23:32:58 +0000

    Add 4-strpbrk.c

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: character array to check
+ * @accept: character array to check for
+ * Return: a pointer to the byte in s that matches or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for  (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (0);
+}
