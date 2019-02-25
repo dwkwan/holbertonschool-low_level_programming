c90e420b8f9f2969bfde8735e0c8846e471ad540
dwkwan <dwkwan1989@gmail.com>
2019-02-25 22:28:03 +0000

    Change to array notation for 2-strchr.c

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
  * _strchr -  locates a character in a string
- * @s: string to check
  * @c: character to locate
+ * @b: character array to check
  * Return: the pointer to first occurrence of c or return NULL
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
+        int i;
+        for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) == c)
-			return (s + i);
+		if (s[i] == c)
+			return s + i;
 	}
-	return (0);
+        return 0;
 }
