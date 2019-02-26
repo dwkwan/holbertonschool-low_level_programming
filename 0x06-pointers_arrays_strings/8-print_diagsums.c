39142d185e3ca1dccf2d1cd3d8e35504a2a80f74
dwkwan <dwkwan1989@gmail.com>
2019-02-26 06:23:11 +0000

    Add 8-print_diagsums.c and update holberton.h

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: square matrix to find sum of
+ * @size: size of square matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int j, m;
+	int diagright = 0;
+	int diagleft = 0;
+
+	for (m = 0; m < (size * size); m = m + (size + 1))
+		diagleft += a[m];
+	for (j = size - 1; j < (size * size) - 1; j = j + (size - 1))
+		diagright += a[j];
+	printf("%i, %i \n", diagleft, diagright);
+}
