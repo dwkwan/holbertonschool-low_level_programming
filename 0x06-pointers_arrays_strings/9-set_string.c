fa44b3b13144e46e67bf4659e5830e69fa138923
dwkwan <dwkwan1989@gmail.com>
2019-02-26 07:57:09 +0000

    Add 9-set_string.c

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - set value
+ * @s: square matrix to find sum of
+ * @to: size of square matrix
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
