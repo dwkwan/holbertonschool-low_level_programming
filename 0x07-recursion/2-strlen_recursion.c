b08c7481bbbeefb8f0ebcb66072628e5dd5b6bb0
dwkwan <dwkwan1989@gmail.com>
2019-02-27 21:43:06 +0000

    Add 2-strlen_recursion.c holberton.h

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to find the length of
+ *
+ * Return: length of string or return 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
