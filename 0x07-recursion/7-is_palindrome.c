76de7e83cb4e7cf77519b229e0eb96de407d7f54
dwkwan <dwkwan1989@gmail.com>
2019-02-28 07:10:20 +0000

    Add 7-is_palindrome.c and update holberton.h

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+int _strlen_recursion(char *s);
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to find the length of
+ *
+ * Return: length of string or return 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+int compare(int length, char *s, int i);
+/**
+ * compare - compares elements in a string
+ * @s: string for comparision
+ * @length: length of string
+ * @i: index
+ * Return: return 1, otherwise 0
+ */
+int compare(int length, char *s, int i)
+{
+	if (s[i] != s[length - i])
+		return (0);
+	if (i == length)
+		return (1);
+	return (compare(length, s, i + 1));
+}
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (compare(_strlen_recursion(s) - 1, s, 0));
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int check(int root, int n);
 int is_prime_number(int n);
 int checkprime(int i, int n);
+int is_palindrome(char *s);
+int compare(int length, char *s, int i);
