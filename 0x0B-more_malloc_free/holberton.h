a903147fd82ec00a772ccfc5e942eb7d5b332a67
dwkwan <dwkwan1989@gmail.com>
2019-03-07 23:24:35 +0000

    First commit

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -11,5 +11,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* HOLBERTON_H */
