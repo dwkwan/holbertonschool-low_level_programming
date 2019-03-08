498ee4819f5fe6d72492b85a3814a05872999ed2
dwkwan <dwkwan1989@gmail.com>
2019-03-08 05:54:58 +0000

    Move conditional statement

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -12,19 +12,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newptr;
 
+	if (ptr == NULL)
+		return (malloc(new_size));
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (new_size == old_size && ptr != NULL)
+	if (new_size == old_size)
 		return (ptr);
 	newptr = malloc(new_size);
 	if (newptr == NULL)
 		return (NULL);
-	if (ptr == NULL)
-		return (newptr);
 	_memcpy(newptr, ptr, old_size);
 	free(ptr);
 	return (newptr);
