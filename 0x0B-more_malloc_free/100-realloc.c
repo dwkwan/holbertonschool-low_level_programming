a9efda48e13cd62cfb84b9c8537994b9b53acef2
dwkwan <dwkwan1989@gmail.com>
2019-03-08 05:35:28 +0000

    Place conditional statement above previous conditional statement

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -10,15 +10,15 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *newptr;
+	char *newptr;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
 	newptr = malloc(new_size);
 	if (newptr == NULL)
 		return (NULL);
