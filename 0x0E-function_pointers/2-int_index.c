846e6fb473d28fb37f4f6d876ef8f9344e6c7368
dwkwan <dwkwan1989@gmail.com>
2019-03-13 19:59:08 +0000

    Modify if statement to account for size being less than 0 and return -1

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,17 +11,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, result;
 
-	if (array != NULL && cmp != NULL)
-	{
-		if (size < 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-		{
-			result = cmp(array[i]);
-			if (result != 0)
-				return (i);
-		}
+	if ((array == NULL ||  cmp == NULL) || size < 0)
 		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+			return (i);
 	}
-	return (0);
+	return (-1);
 }
