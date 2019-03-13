f39d7517c46f7f2c8d2bebfa26d88e6e2475268d
dwkwan <dwkwan1989@gmail.com>
2019-03-13 19:47:59 +0000

    Add brackets for if statement

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,14 +12,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i, result;
 
 	if (array != NULL && cmp != NULL)
-
-	if (size < 0)
-		return (-1);
-	for (i = 0; i < size; i++)
 	{
-		result = cmp(array[i]);
-		if (result != 0)
-			return (i);
+		if (size < 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+			if (result != 0)
+				return (i);
+		}
+		return (-1);
 	}
-	return (-1);
 }
