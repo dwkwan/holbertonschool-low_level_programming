ecee6129511afe27c3dd9447fd477af2249a435b
dwkwan <dwkwan1989@gmail.com>
2019-03-15 16:41:42 +0000

    Modify condition for loop so that it iterating once s1 is equal to 0

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	if (s1 == s2)
 		return (0);
 
-	for (c = 0; s1[c] == s2[c]; c++)
+	for (c = 0; s1[c] == s2[c] && s1[c] != 0; c++)
 	{}
 	if (s1[c] > s2[c])
 		return (s1[c] - s2[c]);
