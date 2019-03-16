71d038b551ebcb749287ff56492f0ade13114eb7
dwkwan <dwkwan1989@gmail.com>
2019-03-16 21:19:52 +0000

    Print last argument within loop

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,14 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
+		if (separator != NULL && i != n - 1)
 			printf("%i%s", va_arg(ap, int), separator);
 		else
 			printf("%i", va_arg(ap, int));
 	}
-			printf("%i", va_arg(ap, int));
 	printf("\n");
 	va_end(ap);
 }
