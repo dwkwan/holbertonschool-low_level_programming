d31876f32c2d3b7ff04e7478efb71dabbf9795cf
dwkwan <dwkwan1989@gmail.com>
2019-03-16 20:00:20 +0000

    Add betty style

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,22 +1,37 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+/**
+ * _charp - prints a character
+ * @list: argument pointer variable, character to print
+ */
 void _charp(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
-
+/**
+ * _intp - prints an integer
+ * @list: argument pointer variable, integer to print
+ */
 void _intp(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
-
+/**
+ * _floatp - prints a float
+ * @list: argument pointer variable, float to print
+ */
 void _floatp(va_list list)
 {
 	printf("%f", va_arg(list, double));
 }
+/**
+ * _stringp - prints a string
+ * @list: argument pointer variable, string to print
+ */
 void _stringp(va_list list)
 {
 	char *s;
+
 	s = va_arg(list, char*);
 	if (s == NULL)
 	{
@@ -43,16 +58,16 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	int j = 0;
 	va_list list;
-
 	char *separator = "";
+
 	va_start(list, format);
 
-	while(format != NULL && format[i])
+	while (format != NULL && format[i])
 	{
 		j = 0;
 		while (types[j].type)
 		{
-			if(format[i] == *types[j].type)
+			if (format[i] == *types[j].type)
 			{
 				printf("%s", separator);
 				types[j].func(list);
