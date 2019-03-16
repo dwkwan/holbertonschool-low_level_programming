ac3730cbcdd12ea78006865de1a9df3c82eb9ce5
dwkwan <dwkwan1989@gmail.com>
2019-03-16 21:08:17 +0000

    Print last argument outside of loop so that it doesn't include separator

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,10 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
+	{
 		if (separator != NULL)
 			printf("%i%s", va_arg(ap, int), separator);
 		else
+			printf("%i", va_arg(ap, int));
+	}
 			printf("%i", va_arg(ap, int));
 	printf("\n");
 	va_end(ap);
