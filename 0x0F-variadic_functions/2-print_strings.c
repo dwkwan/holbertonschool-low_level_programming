3f1c828f3534fe1c9f359477427e5ab92d50e45e
dwkwan <dwkwan1989@gmail.com>
2019-03-16 20:11:28 +0000

    Print newline at end of functions

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -28,9 +28,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			string  = va_arg(ap, char *);
 			if (string != NULL)
-				printf("%s\n", string);
+				printf("%s", string);
 			else
-				printf("(nil)\n");
+				printf("(nil)");
 		}
 	}
+	printf("\n");
 }
