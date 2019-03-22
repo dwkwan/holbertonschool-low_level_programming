032de4b027db2454c3a249da5ce288264a6273fb
dwkwan <dwkwan1989@gmail.com>
2019-03-22 00:18:48 +0000

    Fix typo in comment section

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /**
  * list_len - Returns the number of elements in a linked list_t list
- * @h: a variable pointing to a struct of type list_list
+ * @h: a variable pointing to a struct of type list_t
  *
  * Return: number of elements in linked list
  */
