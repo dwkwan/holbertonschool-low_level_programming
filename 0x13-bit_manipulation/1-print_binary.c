c505d2fad056ab3b69d8c83fbcf3dc139ce96e91
dwkwan <dwkwan1989@gmail.com>
2019-03-28 19:08:45 +0000

    Add 0-binary_to_uint.c, 1-print_binary.c README and header file

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int mask = 32768;
+	int flag = 0;
+
+	if (n == 0)
+	{
+		_putchar ('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag)
+				_putchar('0');
+		}
+		else
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		mask = mask >> 1;
+	}
+}
