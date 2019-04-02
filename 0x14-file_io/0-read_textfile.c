f34f464bb08410650a3e053078e33e3c37c9db31
dwkwan <dwkwan1989@gmail.com>
2019-04-02 19:34:49 +0000

    Change amount of memory allocated

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY, 0400);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(letters + 1);
+	buffer = malloc(letters);
 	if (buffer == NULL)
 		return (0);
 	readcount = read(fd, buffer, letters);
