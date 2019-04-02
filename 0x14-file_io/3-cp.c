d0dd6f899ad25930e954c3b7caef597b934797b5
dwkwan <dwkwan1989@gmail.com>
2019-04-02 23:53:20 +0000

    Change error message

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int ac, char **av)
 {
 	int closestatus, closestatus2, readcount, writecount, fd, fd2;
-	char buffer[1024];
+	char buffer[BUFSIZ];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while ((readcount = read(fd, buffer, 1024)) > 0)
+	while ((readcount = read(fd, buffer, BUFSIZ)) > 0)
 	{
 		writecount = (write(fd2, buffer, readcount));
 		if (writecount != readcount)
@@ -32,13 +32,13 @@ int main(int ac, char **av)
 	closestatus = close(fd);
 	if (closestatus == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	closestatus2 = close(fd2);
 	if (closestatus2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
