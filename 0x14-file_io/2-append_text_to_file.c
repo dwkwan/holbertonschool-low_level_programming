8be0d74dcd50f1007f3e34278302c15cd58ee384
dwkwan <dwkwan1989@gmail.com>
2019-04-02 21:02:09 +0000

    Split up conditions to handle things differently if filename or text_content are NULL

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -13,8 +13,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t writecount;
 	int fd;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+	    if (text_content == NULL)
+		return (1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
