fe9b10aa1c8d488f9b3c4bc5316523757ede7a83
dwkwan <dwkwan1989@gmail.com>
2019-04-02 23:22:57 +0000

    Move write function call outside of if statement

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -3,31 +3,25 @@
  * main - copies the content of a file to another file
  * @ac: number of arguments
  * @av: arguments
- * Return: 1 on success , exit 98, 99, 100 on error
+ * Return: 1 on success , exit 97, 98, 99, 100 on error
  */
 int main(int ac, char **av)
 {
-	int closestatus, closestatus2, readcount, fd, fd2;
+	int closestatus, closestatus2, readcount, writecount, fd, fd2;
 	char buffer[BUFSIZ];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((readcount = read(fd, buffer, BUFSIZ)) > 0)
-		if ((fd2 == -1 || (write(fd2, buffer, readcount) != readcount)))
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+	{
+		writecount = (write(fd2, buffer, readcount));
+		if ((fd2 == -1 || writecount != readcount))
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 	if (readcount == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
