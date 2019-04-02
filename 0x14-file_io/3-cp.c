7a703f07807ebb49eb3d7aa1eb4f72efe6808fe5
dwkwan <dwkwan1989@gmail.com>
2019-04-02 23:30:08 +0000

    Add 3-cp.c

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -16,10 +16,12 @@ int main(int ac, char **av)
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((readcount = read(fd, buffer, BUFSIZ)) > 0)
 	{
 		writecount = (write(fd2, buffer, readcount));
-		if ((fd2 == -1 || writecount != readcount))
+		if (writecount != readcount)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (readcount == -1)
@@ -30,13 +32,13 @@ int main(int ac, char **av)
 	closestatus = close(fd);
 	if (closestatus == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%i\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd);
 		exit(100);
 	}
 	closestatus2 = close(fd2);
 	if (closestatus2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd2);
 		exit(100);
 	}
 	return (0);
