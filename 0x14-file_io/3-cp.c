7b5f82f7f4ef29f95f4cda81deeeadafe27cf106
dwkwan <dwkwan1989@gmail.com>
2019-04-02 23:49:34 +0000

    Change size of array for buffer

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int ac, char **av)
 {
 	int closestatus, closestatus2, readcount, writecount, fd, fd2;
-	char buffer[BUFSIZ];
+	char buffer[1024];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while ((readcount = read(fd, buffer, BUFSIZ)) > 0)
+	while ((readcount = read(fd, buffer, 1024)) > 0)
 	{
 		writecount = (write(fd2, buffer, readcount));
 		if (writecount != readcount)
