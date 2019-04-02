ed789d73fafc335b80105c9e0231e60ee69593a5
dwkwan <dwkwan1989@gmail.com>
2019-04-02 20:01:35 +0000

    Add condition text_content being NULL and modify number of bytes write system call writes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,12 +15,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[i] != 0)
 		i++;
-	writecount = write(fd, text_content, i + 1);
+	writecount = write(fd, text_content, i);
 	if (writecount == -1)
 		return (-1);
 	close(fd);
