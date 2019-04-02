99ec47c42adb49dfb71acc449d9c1eb5ea5ba28a
dwkwan <dwkwan1989@gmail.com>
2019-04-02 19:26:14 +0000

    First commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * read_textfile - Reads a text file and prints iti to the POSIX stdout
+ * @filename - text file to read
+ * @letters - number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t readcount = 0;
+	ssize_t writecount = 0;
+
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY, 0400);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+		return (0);
+	readcount = read(fd, buffer, letters);
+	if (readcount == -1)
+	{
+		free (buffer);
+		return (0);
+	}
+	writecount = write(STDIN_FILENO, buffer, readcount);
+	if (writecount == -1)
+	{
+		free (buffer);
+		return (0);
+	}
+	close (fd);
+	free (buffer);
+	return (writecount);
+}
