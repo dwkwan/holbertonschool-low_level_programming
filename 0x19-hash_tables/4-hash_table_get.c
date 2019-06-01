fd05c44e7c101cff38a8f19dbbbd2002908b9e97
David Kwan <dwkwan1989@gmail.com>
2019-06-01 23:16:59 +0000

    Check for key, empty string, and null pointer

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp = NULL;
 	unsigned long int hash = 0;
 
-	if (!key)
+	if (!key || !*key || !ht)
 		return (NULL);
 	hash = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[hash];
