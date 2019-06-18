64aee3caf267ef93948d70903ab923328259a6b8
David Kwan <dwkwan1989@gmail.com>
2019-06-18 21:04:53 +0000

    Refactor task 1

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,145 +1,39 @@
 #include "sort.h"
 /**
  * insertion_sort_list - sorts a doubly linked list  of integers in ascending
- * order using the Insertion sort algorithm
- *
- * @list: double pointer to doubly-linked list
- *
- * Return: Nothing
- */
+* order using the Insertion sort algorithm
+*
+* @list: double pointer to doubly-linked list
+*
+* Return: Nothing
+*/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL;
-	listint_t *tmp = NULL;
-	listint_t *hold = NULL;
-	int result = 0;
+	listint_t *curr = NULL, *last = NULL, *tmp = NULL;
 
-	if (!(*list))
+	if (!list || !(*list) || !(*list)->next)
 		return;
-	current = (*list)->next;
-	if (checklist(list, &current) == 1)
-		return;
-	while (current)
+
+	curr = (*list)->next;
+	while (curr)
 	{
-		tmp = current->prev;
-		checkend(&current, &tmp, *list);
-		hold = current->next;
-		while (tmp->n > current->n)
+		last = curr->prev;
+		tmp = curr->next;
+		while (last && curr->n < last->n)
 		{
-			result = checkbeg(&current, &tmp, &hold, list);
-			if (result == 1)
-				continue;
-			if (result == 2)
-				return;
-			if (checkjuice(&current, &tmp, *list) == 1)
-				continue;
+			if (last->prev)
+				last->prev->next = curr;
+			last->next = curr->next;
+			curr->next = last;
+			curr->prev = last->prev;
+			last->prev = curr;
+			if (last->next)
+				last->next->prev = last;
+			if (!curr->prev)
+				(*list) = curr;
+			print_list(*list);
+			last = curr->prev;
 		}
-		current = hold;
+		curr = tmp;
 	}
-}
-/**
- * checklist - checks the length of list
- * @list: double pointer to doubly-linked list
- * @current: double pointer to current node
- *
- * Return: 0 or 1
- */
-int checklist(listint_t **list, listint_t **current)
-{
-	if (!(*list)->next)
-		return (1);
-	if (!(*current)->next)
-	{
-		if ((*current)->n > (*list)->n)
-			return (1);
-		(*current)->prev = NULL;
-		(*list)->next = NULL;
-		(*current)->next = *list;
-		(*list)->prev = *current;
-		(*list) = *current;
-		print_list(*list);
-		return (1);
-	}
-	return (0);
-}
-/**
- * checkend - checks end of list for swapping
- * @list: pointer to doubly-linked list
- * @current: double pointer to current node
- * @tmp: double pointer to hold spot of node
- *
- * Return: nothing
- */
-void  checkend(listint_t **current, listint_t **tmp, listint_t *list)
-{
-	if (!(*current)->next && (*tmp)->n > (*current)->n)
-	{
-		(*current)->prev = (*tmp)->prev;
-		(*tmp)->next = (*current)->next;
-		(*tmp)->prev->next = *current;
-		(*tmp)->prev = *current;
-		(*current)->next = *tmp;
-		print_list(list);
-		*tmp = (*current)->prev;
-	}
-}
-/**
- * checkbeg - checks beginning of list
- * @list: double pointer to doubly-linked list
- * @current: pointer to current node
- * @hold: double pointer to position to hold
- * @tmp: double pointer to node
- * Return: 0 or 1
- */
-int checkbeg(listint_t **current, listint_t **tmp,
-	     listint_t **hold, listint_t **list)
-{
-	if ((*tmp)->prev == NULL && (*tmp)->n > (*current)->n)
-	{
-		(*current)->prev = (*tmp)->prev;
-		(*tmp)->next = (*current)->next;
-		(*tmp)->prev = *current;
-		(*current)->next->prev = *tmp;
-		(*current)->next = *tmp;
-		*list = *current;
-		print_list(*list);
-		if (!(*hold)->prev)
-		{
-			*hold = (*tmp)->next;
-			*current = *tmp;
-			*tmp = (*current)->prev;
-			return (2);
-		}
-		*current = *hold;
-		return (1);
-	}
-	return (0);
-}
-/**
- * checkjuice - swaps middle nodes
- * @list:pointer to doubly-linked list
- * @current: double pointer to current node
- * @tmp: double pointer to position to hold
- *
- * Return: 0 or 1
- */
-int checkjuice(listint_t **current, listint_t **tmp,
-		listint_t *list)
-{
-	(*current)->prev = (*tmp)->prev;
-	(*tmp)->next = (*current)->next;
-	(*tmp)->prev->next = *current;
-	(*tmp)->prev = *current;
-	if (!(*tmp)->next && (*current)->n < (list)->n)
-	{
-		(*current)->next = *tmp;
-		*tmp = (*current)->prev;
-		print_list(list);
-		return (1);
-	}
-	(*current)->next->prev = *tmp;
-	(*current)->next = *tmp;
-	print_list(list);
-	*tmp = (*current)->prev;
-	return (0);
 }
