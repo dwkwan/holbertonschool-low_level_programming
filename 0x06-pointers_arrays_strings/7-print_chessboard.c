79ff1a3870be44253148c85d2d5f005ffae8b2c3
David Kwan <dwkwan1989@gmail.com>
2019-07-09 20:50:31 +0000

    Refactor to use one loop

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -5,25 +5,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j, k, m, n, p;
+	int i, j;
 
-	for (i = 0; i < 2; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 		_putchar('\n');
 	}
-	for (k = 2; k < 6; k++)
-	{
-		for (m = 0; m < 8; m++)
-			_putchar(a[k][m]);
-		_putchar ('\n');
-	}
-	for (n = 6; n < 8; n++)
-	{
-		for (p = 0; p < 8; p++)
-			_putchar(a[n][p]);
-		_putchar ('\n');
-	}
-
 }
