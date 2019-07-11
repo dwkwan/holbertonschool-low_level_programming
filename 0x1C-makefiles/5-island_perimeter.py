64dff4b7f05efcffcaebf7afef62496fcd674d31
David Kwan <dwkwan1989@gmail.com>
2019-07-11 22:06:10 +0000

    Accomodate for edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,10 +6,14 @@ def island_perimeter(grid):
             if grid[row][zone] == 1:
                 if zone < len(grid[row]) - 1 and grid[row][zone + 1] == 0:
                     perimeter = perimeter + 1
-                if grid[row][zone - 1] == 0:
+                if zone != 0 and grid[row][zone - 1] == 0:
                     perimeter = perimeter + 1
                 if grid[row - 1][zone] == 0:
                     perimeter = perimeter + 1
                 if row < len(grid) - 1 and grid[row + 1][zone] == 0:
+                    perimeter = perimeter + 1
+                if row == 0 or row == len(grid) - 1:
+                    perimeter = perimeter + 1
+                if zone == 0 or zone == len(grid[row]) - 1:
                     perimeter = perimeter + 1
     return perimeter
