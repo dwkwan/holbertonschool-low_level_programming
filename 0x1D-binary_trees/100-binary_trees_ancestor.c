5435eca9129f9d9e0b76eb46a739fe17cedc6dd4
David Kwan <dwkwan1989@gmail.com>
2019-07-24 22:35:10 +0000

    If nodes are same, return node

--- 0x1D-binary_trees/100-binary_trees_ancestor.c
+++ 0x1D-binary_trees/100-binary_trees_ancestor.c
@@ -29,9 +29,10 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
 	binary_tree_t *first_1 = (binary_tree_t *)first;
-	binary_tree_t *second_2 = (binary_tree_t *)second;
-	binary_tree_t *tmp = NULL;
+	binary_tree_t *second_2 = (binary_tree_t *)second, *tmp = NULL;
 
+	if (first == second)
+		return (first_1);
 	if (!first || !second)
 		return (NULL);
 	if (first->parent == second->parent)
