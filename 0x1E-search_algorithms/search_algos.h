075d84d019390218aa020d888ab2d6940bf2cd02
David Kwan <dwkwan1989@gmail.com>
2019-08-28 00:45:44 +0000

    Add prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,13 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_recur(int *array, size_t start, size_t end, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int interpolation_recur(int *array, size_t start, size_t end, int value);
 
 #endif /* SEARCH_ALGOS_H */
